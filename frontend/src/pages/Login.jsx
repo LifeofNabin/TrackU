b2a176efbba3947a665b8a98103732f093074c6e
Nabin Dhamala <dhamala.nabin1@gmail.com>
2025-08-17 14:09:20 +0530

    Completed frontend UI/UX work

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import LoginForm from "../components/Auth/LoginForm";
-import NavBar from "./components/NavBar";
+import NavBar from "../components/NavBar";
 
 const Login = () => {
   const navigate = useNavigate();
